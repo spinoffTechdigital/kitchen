36e0b15c9263365ee508fa3dec01606d9e8e0f09
spinoffTech <140712585+spinoffTech@users.noreply.github.com>
2023-08-12 15:55:00 +0530

    This is just for branch test

---
+++ kitchen/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Welcome to our Finike kitchen react app</h1>
+    </div>
+  );
+}
+
+export default App;
