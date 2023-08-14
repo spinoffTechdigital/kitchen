162821719f701c112d035fe18ea883ee7b4062f8
spinoffTech <140712585+spinoffTech@users.noreply.github.com>
2023-08-14 18:57:23 +0530

    working on icons

--- kitchen/src/App.js
+++ kitchen/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
 function App() {
   return (
     <div className="App">
--- kitchen/src/components/Footer.js
+++ kitchen/src/components/Footer.js
@@ -1,15 +1,69 @@
-import React from 'react';
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faGoogle,
+  faInstagram,
+  faLinkedinIn,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 const Footer = () => {
   const year = new Date().getFullYear();
 
-  return <footer>
-
-    {`Copyright © Finike House ${year}`}
-    </footer>;
+  return (
+    <footer>
+      <div className="co">
+        <ul>
+          <li>  <a
+            className="btn btn-outline-light btn-floating m-1 g"
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a></li>
+          <li>  <a
+            className="btn btn-outline-light btn-floating m-1 g"
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a></li>
+          <li> <a
+            className="btn btn-outline-light btn-floating m-1 g"
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faGoogle} />
+          </a></li>
+          <li>    <a
+            className="btn btn-outline-light btn-floating m-1 g"
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a></li>
+          <li>      <a
+            className="btn btn-outline-light btn-floating m-1 g"
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a></li>
+          <li>
+          <a
+            className="btn btn-outline-light btn-floating m-1 g"
+            href="#!"
+            role="button"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          </li>
+        </ul>
+      </div>
+      {`Copyright © Finike House ${year}`}
+    </footer>
+  );
 };
 
-
-
 export default Footer;
-
